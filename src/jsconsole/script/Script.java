827863fe0ab9f02bc4b20058cce7fcb42bf6692a
dmac <dmac100@users.noreply.github.com>
2014-06-05 01:24:42 +0100

    Moved to Nashorn and ScriptEngine.

--- src/jsconsole/script/Script.java
+++ src/jsconsole/script/Script.java
@@ -2,30 +2,26 @@ package jsconsole.script;
 
 import java.io.PrintStream;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 import jsconsole.util.Callback;
 
-import org.mozilla.javascript.Context;
-import org.mozilla.javascript.ImporterTopLevel;
-
 public class Script {
-	private ImporterTopLevel scope;
-
+	private final ScriptEngine engine;
+	
 	public Script() {
-		Context context = Context.enter();
-		try {
-			this.scope = new ImporterTopLevel(context);
-		} finally {
-			Context.exit();
+		engine = new ScriptEngineManager().getEngineByName("nashorn");
+		if(engine == null) {
+			throw new RuntimeException("Can't create JavaScript engine");
 		}
-		
-		eval("importPackage(java.lang)");
-		eval("importPackage(java.util)");
 		
 		eval("function print() { System.out.println([].slice.call(arguments).join(', ')) }");
 	}
 	
 	public void addVariable(String name, Object value) {
-		scope.defineProperty(name, value, 0);
+		engine.put(name, value);
 	}
 	
 	public String eval(String command) {
@@ -34,40 +30,36 @@ public class Script {
 			}
 		};
 		
-		return eval(command, nullCallback, nullCallback);
+		try {
+			return eval(command, nullCallback, nullCallback);
+		} catch(ScriptException e) {
+			throw new RuntimeException("Error evaluating command", e);
+		}
 	}
 	
-	public String eval(String command, Callback<String> outputCallback, Callback<String> errorCallback) {
-		Context context = Context.enter();
-		try {
-			context.setOptimizationLevel(-1);
-	        context.setLanguageVersion(Context.VERSION_1_8);
+	public String eval(String command, Callback<String> outputCallback, Callback<String> errorCallback) throws ScriptException {
+        PrintStream out = System.out;
+        PrintStream err = System.err;
+        try {
+        	LineReader outputReader = new LineReader(outputCallback);
+        	LineReader errorReader = new LineReader(errorCallback);
+        	
+	        System.setOut(new PrintStream(outputReader.getOutputStream()));
+	        System.setErr(new PrintStream(errorReader.getOutputStream()));
 	        
-	        PrintStream out = System.out;
-	        PrintStream err = System.err;
-	        try {
-	        	LineReader outputReader = new LineReader(outputCallback);
-	        	LineReader errorReader = new LineReader(errorCallback);
-	        	
-		        System.setOut(new PrintStream(outputReader.getOutputStream()));
-		        System.setErr(new PrintStream(errorReader.getOutputStream()));
-			
-				Object output = context.evaluateString(scope, command, "<js>", 1, null);
-				String value = Context.toString(output);
+			Object output = engine.eval("with(new JavaImporter(java.util, java.lang)) { " + command + "}");
+			String value = String.valueOf(output);
 
-				System.out.close();
-				System.err.close();
-				
-				outputReader.waitUntilDone();
-				errorReader.waitUntilDone();
-				
-				return value;
-	        } finally {
-	        	System.setOut(out);
-	        	System.setErr(err);
-	        }
-		} finally {
-			Context.exit();
-		}
+			System.out.close();
+			System.err.close();
+			
+			outputReader.waitUntilDone();
+			errorReader.waitUntilDone();
+			
+			return value;
+        } finally {
+        	System.setOut(out);
+        	System.setErr(err);
+        }
 	}
 }
